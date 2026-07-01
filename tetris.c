e8b262acbdb80f9ec776b6309d2cd78d573f0beb
ankur-kumar22 <kumarankur2292006@gmail.com>
2026-07-01 16:09:36 +0530

    Milestone 5: added all 7 tetromino shapes with random spawning

--- tetris.c
+++ tetris.c
@@ -3,6 +3,10 @@
 #include <stdlib.h>     //for clear terminal
 #include <termios.h>    //for fast keyboard inupt and raw mode
 #include <sys/select.h> // to detect keyboard input without blocking
+#include <time.h>      // for randomness in shapes
+
+#define ROW 20
+#define COL 10
 
 void enable_raw_mode()
 {
@@ -11,8 +15,6 @@ void enable_raw_mode()
     t.c_lflag &= ~(ICANON | ECHO);
     tcsetattr(0, TCSANOW, &t);
 }
-#define ROW 20
-#define COL 10
 
 struct Tetromino // shape
 {
@@ -20,6 +22,80 @@ struct Tetromino // shape
     int row;
     int col;
 };
+
+struct Tetromino shapes[7] = {
+    {
+        // shape O
+        {
+            {0, 0, 0, 0},
+            {0, 1, 1, 0},
+            {0, 1, 1, 0},
+            {0, 0, 0, 0}},
+        0, // row
+        3  // col
+    },
+    {
+        // shape T
+        {
+            {0, 0, 0, 0},
+            {1, 1, 1, 0},
+            {0, 1, 0, 0},
+            {0, 0, 0, 0}},
+        0, // row
+        3  // col
+    },
+    {
+        // shape S
+        {
+            {0, 0, 0, 0},
+            {0, 1, 1, 0},
+            {1, 1, 0, 0},
+            {0, 0, 0, 0}},
+        0, // row
+        3  // col
+    },
+    {
+        // shape Z
+        {
+            {0, 0, 0, 0},
+            {1, 1, 0, 0},
+            {0, 1, 1, 0},
+            {0, 0, 0, 0}},
+        0, // row
+        3  // col
+    },
+    {
+        // shape J
+        {
+            {0, 0, 0, 0},
+            {1, 1, 1, 0},
+            {0, 0, 1, 0},
+            {0, 0, 0, 0}},
+        0, // row
+        3  // col
+    },
+    {
+        // shape L
+        {
+            {0, 0, 0, 0},
+            {1, 1, 1, 0},
+            {1, 0, 0, 0},
+            {0, 0, 0, 0}},
+        0, // row
+        3  // col
+    },
+    {
+        // shape I
+        {
+            {0, 0, 0, 0},
+            {1, 1, 1, 1},
+            {0, 0, 0, 0},
+            {0, 0, 0, 0}},
+        0, // row
+        3  // col
+    },
+};
+
 int board[ROW][COL] = {0};
 
 void drawboard() // empty board
@@ -32,7 +108,7 @@ void drawboard() // empty board
             if (board[i][j] == 1)
                 printf("[]");
             else
-                // printf("░░");
+                //printf("░░");
                 printf("::");
         }
         printf("|\n");
@@ -57,6 +133,7 @@ void draw_tetromino(struct Tetromino t) // printing shape
         }
     }
 }
+
 int kbhit()
 {
     struct timeval tv = {0, 0};
@@ -80,7 +157,8 @@ void erase_tetromino(struct Tetromino t)
         }
     }
 }
-int can_move_down(struct Tetromino t)  //collision detection for bottom wall
+
+int can_move_down(struct Tetromino t) // collision detection for bottom wall
 {
     int i, j;
     for (i = 0; i < 4; i++)
@@ -89,7 +167,7 @@ int can_move_down(struct Tetromino t)  //collision detection for bottom wall
         {
             if (t.shape[i][j] == 1)
             {
-                if (t.row + i + 1 >= ROW)
+                if (t.row + i + 1 >= ROW || board[t.row + i + 1][t.col + j] == 1)
                 {
                     return 0;
                 }
@@ -99,24 +177,19 @@ int can_move_down(struct Tetromino t)  //collision detection for bottom wall
     return 1;
 }
 
+
 int main()
 {
     enable_raw_mode();
-    struct Tetromino current = {
-        {{0, 0, 0, 0},
-         {1, 1, 1, 1},
-         {0, 0, 0, 0},
-         {0, 0, 0, 0}},
-        0, // row
-        3  // col
-    };
+    srand(time(NULL)); // seed random number
+    struct Tetromino current = shapes[rand() % 7];
 
     draw_tetromino(current);
     drawboard();
     while (1) // game control loop
     {
-        erase_tetromino(current);
-        if (kbhit())
+        erase_tetromino(current); //  ghost block prevention by erase before any movement (keyboard or falling)
+        if (kbhit())              // keyboard inputs
         {
             {
                 char key = getchar();
@@ -130,10 +203,10 @@ int main()
                     if (current.col < COL - 4)
                         current.col++;
                 }
-                /**else if (key == 's' || key == 'S')
+                else if (key == 's' || key == 'S')
                 {
                     current.row++;
-                }**/
+                }
                 else if (key == 'q' || key == 'Q')
                 {
                     break;
@@ -143,6 +216,16 @@ int main()
         if (can_move_down(current))
         {
             current.row++;
+        }
+        else
+        {
+            draw_tetromino(current);
+            current = shapes[rand() % 7];
+            current.row = 0;
+            current.col = 3;
+
+            system("clear");
+            drawboard();
         }
         draw_tetromino(current);
         system("clear");
